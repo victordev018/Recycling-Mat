4e8c6e6b64109ecb176d66e8de26b951b2129545
João VIctor <joawrt8520@gmail.com>
2024-06-05 18:05:50 -0300

    feat: adicionada funcionalidade de limitar instanciação de resíduos de acordo com a quantidade

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -4,24 +4,9 @@ class_name Game
 onready var residueScene : PackedScene = preload("res://Scenes/Residue.tscn");
 onready var levelNode : Node2D = get_parent();
 
-## Cria e posiciona uma instância de resíduo no jogo na posição apropriada
-func createResidue(): 
-	print("criando resíduo"); 
-	# instanciar cena de resíduo
-	var _residue = residueScene.instance();
-	levelNode.add_child(_residue);
-	# obter chave aleatória
-	var _residueKey = getRandomResidueKey();
-	# atribuir sprite devido ao resíduo
-	_residue.residueTexture.texture = Global.residueDatabase.get(_residueKey).get("texture");
-	_residue.isRecyclible = Global.residueDatabase.get(_residueKey).get("recyclable");
-	_residue.global_position = Vector2(get_viewport().size.x / 2, get_viewport().size.y + 32);
-	if Input.is_action_just_pressed("ui_right"):
-		_residue.global_position = Vector2(get_viewport().size.x + 32, get_viewport().size.y + 0);
-	elif Input.is_action_just_pressed("ui_left"):
-		_residue.global_position = Vector2(get_viewport().size.x - 32, get_viewport().size.y + 0);
-		
-	
+## array de resíduos que ja foram instanciados:
+var residuesAlreadyInstantiate: Array = [];
+
 func _ready():
 	Global.instanciateResidue = true;
 	
@@ -31,12 +16,34 @@ func _process(delta):
 		createResidue();
 		Global.instanciateResidue = false;
 			
+
+## Cria e posiciona uma instância de resíduo no jogo na posição apropriada
+func createResidue(): 
+	print("criando resíduo"); 
+	# instanciar cena de resíduo
+	var _residue = residueScene.instance();
+	levelNode.add_child(_residue);
+	# obter chave aleatória
+	var _residueKey = getRandomResidueKey();
+	if _residueKey != "":
+		# atribuir sprite devido ao resíduo
+		_residue.residueTexture.texture = Global.residueDatabase.get(_residueKey).get("texture");
+		_residue.isRecyclible = Global.residueDatabase.get(_residueKey).get("recyclable");
+		_residue.global_position = Vector2(get_viewport().size.x / 2, get_viewport().size.y + 32);
+
  ## função para pegar uma chave aleatória do dicionário
 func getRandomResidueKey() -> String:
+	## cria array de string com as chaves do dicionário residueDatabase
 	var _keys = Global.residueDatabase.keys()
+	## verifica se a quantidade de residuos que foram instanciados é igaual a 
+	## quantidade de chaves que temos em nosso array de chaves (_keys)
+	if len(residuesAlreadyInstantiate) == len(_keys):## TODO: indroduza uma função para gerar mais ações quando usar todos os residuos
+		return ""
 	var _index = randi() % len(_keys);
+	## se a chave atual ja foi instanciada, pegue outra que não tenha sido instanciada
+	while (_keys[_index] in residuesAlreadyInstantiate):
+		_index = randi() % len(_keys);
+	## atualiza a lista dos residuos instanciados
+	residuesAlreadyInstantiate.append(_keys[_index])
 	return _keys[_index];
-
-
-
 
