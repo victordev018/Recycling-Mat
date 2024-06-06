aa53187f190aecc274bfdc4bf0fc255ac6fe6ab9
João VIctor <joawrt8520@gmail.com>
2024-06-05 23:03:45 -0300

    feat: Added label Score in game and system of the points

--- Scripts/mainLevel.gd
+++ Scripts/mainLevel.gd
@@ -5,5 +5,11 @@ onready var trayPosition: Vector2 = Vector2(240, 144);
 onready var holeRightPos: Vector2 = Vector2(272, 144);
 onready var holeLeftPos: Vector2 = Vector2(208, 144);
 
+## referência ao label do score
+onready var labelScore: Label = get_node("Score");
+
 func _ready() -> void:
 	Global.levelNode = self;
+
+func _process(delta) -> void:
+	labelScore.text = "Score: " + str(Global.score);
