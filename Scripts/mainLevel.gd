400a45acd79b9b3452e380f6bdb951ef89a1b804
João VIctor <joawrt8520@gmail.com>
2024-06-06 10:19:50 -0300

    feat: Adicionando tempo de escolha para decisão de tipo de resíduo (reciclável/não reciclável)

--- Scripts/mainLevel.gd
+++ Scripts/mainLevel.gd
@@ -8,11 +8,24 @@ onready var holeLeftPos: Vector2 = Vector2(208, 144);
 ## referência ao animatedSprite da esteira
 onready var animation: AnimatedSprite = get_node("AnimatedSprite");
 
-## referência ao label do score
-onready var labelScore: Label = get_node("Score");
+## referência ao label do score e choiceTimer, respectivamente
+onready var labelScore: Label = get_node("Labels/Score");
+onready var labelChoiceTimer: Label = get_node("Labels/ChoiceTimer");
+
+## referência ao tempo de escolha do tipo de resíduo
+onready var choiceTimer: Timer = get_node("ChoiceTimer");
 
 func _ready() -> void:
 	Global.levelNode = self;
 
 func _process(delta) -> void:
 	labelScore.text = "Score: " + str(Global.score);
+	labelChoiceTimer.text = "Timer: " + str(int(choiceTimer.time_left))
+
+## quando o choice timer acabar:
+func _on_ChoiceTimer_timeout():
+	print("Tempo de escolha esgotado ._.")
+	var _points = 5;
+	Global.score -= _points;
+	Global.residuo.queue_free()
+	Global.instanciateResidue = true;
