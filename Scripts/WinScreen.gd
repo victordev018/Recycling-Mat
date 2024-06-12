10927b395fde6272608fdc219bdd586bdb8ac6ec
Felipe Gabriel <felipemrqs07@gmail.com>
2024-06-11 23:13:32 -0300

    feat: win scene

---
+++ Scripts/WinScreen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+onready var label = $Label
+
+func _ready():
+	$Button.grab_focus()
+
+func _process(delta):
+	label.text = "Parabéns!! seu score foi " + str(Global.score) + " pontos"
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Scenes/ResiduesResume.tscn")
